c86efb63708eb20f49da6e2451cd0888a58bef9c
nicolasgonzalez <104945758+nicogz90@users.noreply.github.com>
2023-05-20 10:34:28 -0300

    upload portolio

---
+++ js/app.js
@@ -0,0 +1,121 @@
+/* Changes tab title when user changes tab: */
+const tabTitle = document.title;
+window.addEventListener("blur", () => (document.title = "<NG /> Come back 😔"));
+window.addEventListener("focus", () => (document.title = tabTitle + " 😀"));
+
+/* Toggle Menu for mobile */
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+function toggleMenu() {
+  menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
+
+/* Scroll progress bar */
+const body = document.querySelector("body");
+const bar = document.getElementById("scroll-progress");
+const updateBar = () => {
+  let progress =
+    (window.scrollY / (body.scrollHeight - window.innerHeight)) * 100;
+  bar.style.width = `${progress}%`;
+  requestAnimationFrame(updateBar);
+};
+updateBar();
+
+/* Intersection observer */
+const observerProfileDescription = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+      // if I wanted to do the animation every time the element is observed:
+      // else {
+      //   entry.target.classList.remove("show");
+      // }
+    });
+  }
+);
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observerProfileDescription.observe(el));
+
+observerProfileTitle = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("type");
+    } else {
+      entry.target.classList.remove("type");
+    }
+  });
+});
+
+const typewriterTitle = document.querySelector(".typewriter h3");
+observerProfileTitle.observe(typewriterTitle);
+
+const observerSkills = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry, i) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("addGap");
+      entry.target.style.transition = `.3s linear ${i / 30}s`;
+    } else {
+      entry.target.classList.remove("addGap");
+    }
+  });
+});
+
+const skills = document.querySelectorAll(".skills-list div");
+skills.forEach((skill) => observerSkills.observe(skill));
+
+/* Dark Mode Toggle */
+const toggleBtn = document.getElementById("toggle-btn");
+const theme = document.querySelector("body");
+const header = document.querySelector(".topheader");
+const portfolio = document.querySelector(".logo span");
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const links = document.querySelectorAll("ul.menu li a");
+
+let darkMode = localStorage.getItem("dark-mode");
+
+const enableDarkMode = () => {
+  theme.classList.add("dark-mode-theme");
+  toggleBtn.checked = true;
+  header.classList.add("dark-mode-theme");
+  portfolio.style.color = "var(--theme-color)";
+  openMenu.style.filter = "invert(100%)";
+  closeMenu.style.filter = "invert(100%)";
+  for (const link of links) {
+    link.style.color = "var(--theme-color)";
+  }
+  localStorage.setItem("dark-mode", "enabled");
+};
+
+const disableDarkMode = () => {
+  theme.classList.remove("dark-mode-theme");
+  toggleBtn.checked = false;
+  header.classList.remove("dark-mode-theme");
+  portfolio.style.color = "var(--dark-theme-color)";
+  openMenu.style.filter = "grayscale(100%)";
+  closeMenu.style.filter = "grayscale(100%)";
+  for (const link of links) {
+    link.style.color = "var(--dark-theme-color)";
+  }
+  localStorage.setItem("dark-mode", "disabled");
+};
+
+// set state of darkMode on page load
+darkMode === "enabled" ? enableDarkMode() : disableDarkMode();
+
+toggleBtn.addEventListener("click", (e) => {
+  darkMode = localStorage.getItem("dark-mode");
+  if (darkMode === "disabled") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
